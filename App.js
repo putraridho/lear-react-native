663ca87d52d79dda36a7cda7b5a3353be0407aad
Ridho <muhammadridhoputra@gmail.com>
2020-08-14 20:15:54 +0700

    async storage & react-persist

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react';
 import Home from './Home';
 
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Home />
+      <PersistGate loading={null} persistor={persistor}>
+        <Home />
+      </PersistGate>
     </Provider>
   );
 }
--- store.js
+++ store.js
@@ -1,6 +1,16 @@
 import { createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import AsyncStorage from '@react-native-community/async-storage';
 import { reducer } from './todoListRedux';
 
-const store = createStore(reducer);
+export const store = createStore(
+  persistReducer(
+    {
+      key: 'root',
+      storage: AsyncStorage,
+    },
+    reducer,
+  ),
+);
 
-export default store;
+export const persistor = persistStore(store);
