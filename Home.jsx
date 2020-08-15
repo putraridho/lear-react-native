97d0d0dc24ec031335a1f519fed284d0fa3ed038
Ridho <muhammadridhoputra@gmail.com>
2020-08-15 13:16:00 +0700

    redux thunk

--- Home.jsx
+++ Home.jsx
@@ -1,30 +1,52 @@
-import React from 'react';
-import { StyleSheet, View, StatusBar } from 'react-native';
-
+import React, { useEffect } from 'react';
+import { ScrollView, View, ActivityIndicator, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
-import { actionCreators } from './todoListRedux';
-import Title from './Title';
-import Input from './Input';
-import List from './List';
+import { actionCreators } from './postsRedux';
 
-function Home({ todos, dispatch }) {
-  const onAddTodo = (text) => {
-    dispatch(actionCreators.add(text));
+function Home({ error, loading, posts, dispatch }) {
+  useEffect(() => {
+    dispatch(actionCreators.fetchPosts());
+  }, [dispatch]);
+
+  const refresh = async () => {
+    await dispatch(actionCreators.clearPosts());
+    dispatch(actionCreators.fetchPosts());
   };
 
-  const onRemoveTodo = (index) => {
-    dispatch(actionCreators.remove(index));
-  };
+  const renderPost = ({ id, title, body }, i) => (
+    <View key={id} style={styles.post}>
+      <View style={styles.postNumber}>
+        <Text>{i + 1}</Text>
+      </View>
+      <View style={styles.postContent}>
+        <Text>{title}</Text>
+        <Text style={styles.postBody}>{body}</Text>
+      </View>
+    </View>
+  );
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator animating />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <Text>Failed to load posts!</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
-      <StatusBar />
-      <Title>To-Do List</Title>
-      <Input
-        placeholder="Type a todo, then hit enter!"
-        onSubmitEditing={onAddTodo}
-      />
-      <List list={todos} onPressItem={onRemoveTodo} />
+      <ScrollView style={styles.container}>{posts.map(renderPost)}</ScrollView>
+      <TouchableOpacity style={styles.button} onPress={refresh}>
+        <Text>Refresh</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -33,10 +55,48 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  post: {
+    flexDirection: 'row',
+  },
+  postNumber: {
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  postContent: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    paddingVertical: 25,
+    paddingRight: 15,
+  },
+  postBody: {
+    marginTop: 10,
+    fontSize: 12,
+    color: 'lightgray',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    padding: 15,
+    backgroundColor: 'skyblue',
+  },
+  button: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderTopColor: 'lightgray',
+  },
 });
 
-const mapStateToProps = (state) => ({
-  todos: state.todos,
+const mapStateToProps = ({ loading, error, posts }) => ({
+  loading,
+  error,
+  posts,
 });
 
 export default connect(mapStateToProps)(Home);
--- store.js
+++ store.js
@@ -1,16 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { persistReducer, persistStore } from 'redux-persist';
 import AsyncStorage from '@react-native-community/async-storage';
-import { reducer } from './todoListRedux';
+import thunk from 'redux-thunk';
+import { reducer } from './postsRedux';
 
-export const store = createStore(
-  persistReducer(
-    {
-      key: 'root',
-      storage: AsyncStorage,
-    },
-    reducer,
-  ),
-);
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
 
 export const persistor = persistStore(store);
